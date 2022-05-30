c444ec85b002dcc41fed5132cfa350e9871add80
Cyrreal <lazukinki@gmail.com>
2022-05-31 02:34:45 +0300

    git page

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/frontend-challenge">
       <App />
     </BrowserRouter>
   </React.StrictMode>
